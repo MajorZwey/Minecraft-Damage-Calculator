fa71f15f914fd606e9eb54943c11a0ce0fbb3a5d
majorzwey <maj.rebec@gmail.com>
2026-07-20 12:18:31 +0200

    Minor fixes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class Main {
-     public static void main(String[] args) {
+     public static void Main(String[] args) {
           Scanner scanner = new Scanner(System.in);
 
           System.out.print("What is the damage?: ");
