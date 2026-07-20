96e5cc4672443f59c6485241f8630860dc88f800
majorzwey <maj.rebec@gmail.com>
2026-07-20 12:20:41 +0200

    Minor fixes

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,39 @@
 
 
 import java.util.Scanner;
-
 public class Main {
-     public static void Main(String[] args) {
-          Scanner scanner = new Scanner(System.in);
-
-          System.out.print("What is the damage?: ");
-          double damage = scanner.nextDouble();
-
-          System.out.print("How much armor points? (0-20): ");
-          int armorPoints = scanner.nextInt();
-
-          System.out.print("How much armor toughness? (0-20): ");
-          int toughness = scanner.nextInt();
-
-          double epf = armorPoints - (4.0 * damage) / (toughness + 8);
-          double reduction = Math.min(20, Math.max(armorPoints / 5.0, epf));
-          double damageTaken = damage * (1 - reduction / 25);
-
-          System.out.println("The damage taken without enchants is " + damageTaken);
-
-          System.out.print("How much total Protection EPF? (0-20): ");
-          double protection = Math.min(20, Math.max(0, scanner.nextDouble()));
-
-          double damageTakenProt = damageTaken * (1 - protection / 25);
-          System.out.println("The damage taken with protection is " + damageTakenProt);
-
-          scanner.close();
-     }
-}
+   static void main(String[] args) {
 
 
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the damage?: ");
+        double damage = scanner.nextDouble();
+
+        System.out.print("How much armor points? (1-20) ");
+        int armor_points = scanner.nextInt();
+
+        System.out.print("How much armor toughness? (1-20) ");
+        int toughness = scanner.nextInt();
+
+        double damage_taken = (damage * (1 - Math.min(20, Math.max( armor_points / 5, armor_points - (4 * damage) / (toughness + 8))) / 25));
+        System.out.println("The damage taken without enchants is " + damage_taken);
+
+        System.out.println("How much protection? (1-20) ");
+        double protection = scanner.nextDouble();
+
+        double damage_taken_prot = damage_taken * (1 - protection / 25);
+
+        System.out.println("The damage taken with protection is " + damage_taken_prot);
+
+
+        scanner.close();
+
+
+    }
 
 
 
